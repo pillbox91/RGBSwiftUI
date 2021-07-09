c7bfddb48b994a09fe0e210d529046088813e4dc
Andrey Averyanov <averyanov1996@gmail.com>
2021-07-09 23:19:20 +0300

    Initial Commit

---
+++ RGBSwiftUI/RGBSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBSwiftUIApp.swift
+//  RGBSwiftUI
+//
+//  Created by Андрей Аверьянов on 09.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct RGBSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
